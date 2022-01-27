745eb0d9445ed82bbb4d03739a9ab0fa63d70501
Omkar Bhoir <omkarbhoir878@gmail.com>
2022-01-27 10:53:48 +0530

    Active users chart done

--- src/Components/RightPanel.js
+++ src/Components/RightPanel.js
@@ -3,10 +3,29 @@ import { FaAngleRight } from "react-icons/fa";
 import { AiOutlineCalendar } from "react-icons/ai";
 import { FaDollarSign } from "react-icons/fa";
 import { IoMdFolderOpen } from "react-icons/io";
+import { FiMoreVertical } from "react-icons/fi";
+import { Bar, Line } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js";
 
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend
+);
 export default function RightPanel() {
 	return (
-		<div className="px-3 pt-5 w-full">
+		<div className="px-3 pt-5 w-full flex flex-col gap-5">
 			{/* Top headings */}
 			<TopPart />
 
@@ -19,41 +38,116 @@ export default function RightPanel() {
 	);
 }
 
-
 function ChartsPart() {
+	const data = {
+		// Name of the variables on x-axies for each bar
+		labels: ["", "", "", "", "", "", "", "", "", "", "", ""],
+		datasets: [
+			{
+				// Label for bars
+				label: "as",
+				// Data or value of your each variable
+				data: [
+					100, 150, 120, 300, 310, 320, 150, 200, 210, 180, 170, 160,
+				],
+				// Color of each bar
+				backgroundColor: "#5750ee",
+				// Border color of each bar
+				borderColor: "#5750ee",
+				borderWidth: 0.5,
+				borderRadius: 9999,
+			},
+		],
+	};
 	return (
-		<div className="flex">
+		<div className="flex gap-5">
 			{/* Active users Chart */}
-			<div className="bg-white p-6 w-full border shadow-md rounded-md border-gray-200 ">
-
-				<div className="flex justify-between">
+			<div className="bg-white pt-3 w-full border shadow-md rounded-md border-gray-200 max-w-[300px]">
+				<div className="flex justify-between px-6">
 					<div>
-						<div>
-							Active Users
-
-						</div>
-						<div>
+						<div className="text-lg font-bold">Active Users</div>
+						<div className="text-sm font-light">
 							Page views per second
 						</div>
 					</div>
-					<div>
-						109
-					</div>
+					<div className="font-bold text-lg">109</div>
 				</div>
 
 				{/* Chart will go here */}
-				<div></div>
+				<div
+					className="px-6"
+					style={{ maxHeight: "250px", maxWidth: "300px" }}
+				>
+					<Bar
+						options={{
+							responsive: true,
+							maintainAspectRatio: false,
+							plugins: {
+								legend: {
+									position: "top",
+									display: "none",
+								},
+								title: {
+									display: false,
+								},
+							},
+							scales: {
+								yAxis: [
+									{
+										ticks: {
+											// The y-axis value will start from zero
+											beginAtZero: true,
+										},
+									},
+								],
+							},
+						}}
+						data={data}
+						height={250}
+					/>
+				</div>
 
+				{/* Information about the graph  */}
+				<div className="flex flex-col">
+					<div className="flex px-3 py-3 justify-between border-b border-gray-300">
+						<div className="text-gray-400">/app/projects</div>
+						<div>24</div>
+					</div>
+					<div className="flex px-3 py-3 justify-between border-b border-gray-300">
+						<div className="text-gray-400">/app/chats</div>
+						<div>21</div>
+					</div>
+					<div className="flex px-3 py-3 justify-between border-b border-gray-300">
+						<div className="text-gray-400">/cart</div>
+						<div>15</div>
+					</div>
+					<div className="flex px-3 py-3 justify-between border-b border-gray-300">
+						<div className="text-gray-400">/cart/checkout</div>
+						<div>8</div>
+					</div>
+				</div>
 
-				<div></div>
-				<div></div>
+				{/* See all */}
+				<div className="flex items-center justify-end  gap-2 text-xs py-3 px-6">
+					SEE ALL <FaAngleRight />
+				</div>
 			</div>
 			{/* Performance Over time */}
-			<div className="bg-white p-6 w-full border shadow-md rounded-md border-gray-200 ">
+			<div className="bg-white  w-full border shadow-md shadow-gray-400 rounded-md border-gray-200 ">
+				<div className="flex p-5 justify-between shadow-sm">
+					<div className="text-xl font-semibold">
+						Performance Over Time
+					</div>
+					<div>
+						<FiMoreVertical size={"1.2rem"} />
+					</div>
+				</div>
 
+				{/* Line graph will go here */}
+				<div></div>
 			</div>
 		</div>
-	)
+	);
 }
 
 function TopPart() {
@@ -145,5 +239,6 @@ function TopPart() {
 					</div>
 				</div>
 			</div>
-		</div>);
+		</div>
+	);
 }
